86f470722115f80cf3417986a526b58424edfd2a
Will <willb.can@gmail.com>
2024-11-20 15:34:24 -0500

    updates`

---
+++ wp-content/plugins/plugin1/plugin1.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Plugin 1
+ * Description: An experimental project for tbk.
+ * Version: 1.0.0
+ * Author: William Bertolo
+ * Author URI: https://github.com/wbertolo/
+ * Text Domain: plugin-1
+ * Domain Path: /languages
+ *
+ * @package Plugin1
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Constants
+define( 'PLUGIN_1_VERSION', '1.0.0' );
+define( 'PLUGIN_1__DIR', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_1__URL', plugin_dir_url( __FILE__ ) );
+
+// Includes
+require_once PLUGIN_1__DIR . 'includes/plugin1-functions.php';
+require_once PLUGIN_1__DIR . 'includes/class-plugin1.php';
+
+
+// Initialize the plugin
+function run_PLUGIN_1_() {
+    $plugin = new Plugin1();
+    $plugin->run();
+}
+run_PLUGIN_1_();
---
+++ wp-content/plugins/plugin1/templates/plugin1-template.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template file for Plugin 1
+ *
+ * @package Plugin1
+ */
+?>
+<div class="plugin1">
+    <h2>Welcome to Plugin 1</h2>
+    <p>some text</p>
+</div>
